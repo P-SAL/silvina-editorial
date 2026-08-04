655cfe46497be9838dbe0e3a1c05ac5eaa9ba2ea
P-Sal <plsalonio@gmail.com>
2026-08-04 14:22:28 -0300

    Borrado

--- silvina_editorial_v095/convert_logo.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-
-# Convert SILVINA V08.png to icon
-img = Image.open('assets/SILVINA V08.png')
-img.save('assets/silvina_v08.ico', format='ICO', sizes=[(256, 256)])
-print("✅ Logo SILVINA V08 convertido a .ico")
